cdff1261cacf1ceb18bd5b45ff2104601b3274ac
Ryan Gamel <gcsxl@monsanto.com>
2022-04-11 14:57:10 -0500

    update pre-class for april 22

--- snippets/asyncErrorHandling.js
+++ snippets/asyncErrorHandling.js
@@ -1,7 +1,7 @@
 // in a real project this would be in a different file/module
 const DataService = { 
   // ...
-  retrieveData = () => Promise.resolve({ value: 'someValue'})
+  retrieveData: async () => Promise.resolve({ value: 'someValue'})
   // ...
 }
 
@@ -16,25 +16,27 @@ const getDataForDisplay = async () => {
   setIsLoading(true)
   try {
     const data = await DataService.retrieveData()
+
+    // do something with the data
     console.log(data.value)
   } catch (err) {
     // do something with the error
     console.error(err)
   } finally {
     setIsLoading(false)
-  }  
+  }
 }
 
 // equivalent to this one with Promises:
 const getDataForDisplayWithPromises = () => {
   setIsLoading(true)
-  return DataService.retrieveData
-    .then(data => {
+  return DataService.retrieveData()
+    .then((data) => {
         // do something with data
         console.log(data.value)
     }).catch((err) => {
         // handle error
-        console.log(err)
+        console.error(err)
     }).finally(() => {
         setIsLoading(false)
     })
